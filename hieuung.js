038ddd64ed408394c89b9b910ccf3bd5d6882df7
Lê Thị Kim Ngân <lethikimngan20803@gmail.com>
2025-08-25 14:42:29 +0700

    Update hieuung.js

--- hieuung.js
+++ hieuung.js
@@ -140,5 +140,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
   window.addEventListener('scroll', revealSeoOnScroll);
   window.addEventListener('load', revealSeoOnScroll);
+  let btn = document.getElementById("btnTop");
 
+  window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  };
+
+  btn.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 });
+
